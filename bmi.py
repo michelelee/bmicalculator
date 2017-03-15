45ec382e6fc8567bf23fa66adb74b13e1e913a19
Mish Lee <mishmqlee@gmail.com>
2017-03-14 18:49:11 -0700

    Convert to spaces

--- bmi.py
+++ bmi.py
@@ -4,20 +4,21 @@
 
 
 def bmi(height, weight):
-	height_meters = height * 0.025
-	weight_kg = weight * 0.45
+    height_meters = height * 0.025
+    weight_kg = weight * 0.45
 
-	if height_meters == 0:
-		return 0
+    if height_meters == 0:
+        return 0
 
-	return round(weight_kg / height_meters**2, 1)
+    return round(weight_kg / height_meters**2, 1)
+
 
 def test():
-	assert bmi(63, 125) == 22.7
-	assert bmi(0, 125) == 0
+    assert bmi(63, 125) == 22.7
+    assert bmi(0, 125) == 0
 
-	print('All Test Passed')
+    print('All Test Passed')
 
 
 if __name__ == '__main__':
-	test()
+    test()
